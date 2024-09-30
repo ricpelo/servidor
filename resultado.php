85767bb1d1b467e10c4c1cc2ab9e3439222badbc
Ricardo Pérez <ricpelo@gmail.com>
2024-09-30 20:26:34 +0200

    Con sintaxis alternativa

--- resultado.php
+++ resultado.php
@@ -1,37 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultado</title>
 </head>
+
 <body>
-    <p>
-        <?php
-        if (isset($_GET['op1']) && !empty($_GET['op1']) && is_numeric($_GET['op1'])) {
-            $op1 = trim($_GET['op1']);
+    <?php
+    $errores = [];
+
+    if (isset($_GET['op1'])) {
+        $op1 = trim($_GET['op1']);
+        if (empty($op1)) {
+            $errores[] = "El primer operando es obligatorio.";
         } else {
-            echo "<h3>El primer operando no es correcto.</h3>";
+            if (!is_numeric($op1)) {
+                $errores[] = "El primer operando no es un número.";
+            }
         }
+    }
 
-        if (isset($_GET['op2']) && !empty($_GET['op2']) && is_numeric($_GET['op2'])) {
-            $op2 = trim($_GET['op2']);
+    if (isset($_GET['op2'])) {
+        $op2 = trim($_GET['op2']);
+        if (empty($op2)) {
+            $errores[] = "El segundo operando es obligatorio.";
         } else {
-            echo "<h3>El segundo operando no es correcto.</h3>";
+            if (!is_numeric($op2)) {
+                $errores[] = "El segundo operando no es un número.";
+            }
         }
+    }
 
-        if (isset($_GET['op']) && !empty($_GET['op'])) {
-
-
-        if (isset($op1, $op2)) {
-            $res = $op1 + $op2;  // int|float
-
-            $mensaje = "La suma de $op1 y $op2 es $res.";
-            ?>
-            <?= $mensaje ?>
-        <?php
+    if (isset($_GET['op'])) {
+        $op = trim($_GET['op']);
+        if (empty($op)) {
+            $errores[] = "La operación es obligatoria.";
+        } else {
+            if (!in_array($op, ['+', '-', '*', '/'])) {
+                $errores[] = "Operación incorrecta.";
+            }
         }
-        ?>
-    </p>
+    }
+
+    if (!empty($errores)):
+        foreach ($errores as $error): ?>
+            <h3><?= $error ?></h3><?php
+        endforeach;
+    else:
+        switch ($op):
+            case '+':
+                $res = $op1 + $op2;
+                break;
+            case '-':
+                $res = $op1 - $op2;
+                break;
+            case '*':
+                $res = $op1 * $op2;
+                break;
+            case '/':
+                $res = $op1 / $op2;
+                break;
+        endswitch; ?>
+        <p>La operación <?= $op1 ?> <?= $op ?> <?= $op2 ?> vale <?= $res ?>.</p><?php
+    endif; ?>
 </body>
+
 </html>
